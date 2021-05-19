0c60c9fedf29fb32fb7d228581606be3898b024b
OrestisNer <orestis@ep-mail.gr>
2021-05-20 02:39:50 +0300

    Export all packages from index.js in order to be more easy to import from other services

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-interface Color {
-  red: Number;
-  blue: Number;
-  green: Number;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color: Color = {
-  red: 10,
-  blue: 10,
-  green: 10
-};
-
-
-export default color;
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
