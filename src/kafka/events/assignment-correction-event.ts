704d4444d179456c19d493a5ed79276e666b0edd
Orestis <orestis@ep-mail.gr>
2021-08-04 21:15:27 +0300

    Change status type in AssignmentCorrectionEvent

--- src/kafka/events/assignment-correction-event.ts
+++ src/kafka/events/assignment-correction-event.ts
@@ -1,16 +1,11 @@
 import { Topics } from '../topics';
 
-enum AssignmentSubmitType {
-    "failed",
-    "success"
-}
-
 export interface AssignmentCorrectionEvent {
     topic: Topics.AssignmentCorrectionTopic;
     data: {
         assignmentId: string;
         userId: string;
-        status: AssignmentSubmitType;
+        status: string;
         result: string;
     }
 }
